1dde48c200999dd035548559015869691924f370
Matteo <m.buscaroli@outlook.com>
2019-04-22 16:02:41 +0100

    Added BST.c (Binary Search Tree) - Deletion not yet implemented

--- DataStructures/BST.c
+++ DataStructures/BST.c
@@ -2,10 +2,9 @@
  * IMPLEMENTATION OF THE DATA STRUCTURE AND
  * ASSOCIATED FUNCTIONS
  *
- * Also Using the Circular QUeue Data Structure to
- * enter the tree nodes by hand
- *
  * Matteo Buscaroli
+ *
+ * ----- TO DO : BST_DELETE ------
  */
 
 #include <stdio.h>
@@ -19,77 +18,8 @@ struct Node {
     struct Node *rchild;
 };
 
-struct Queue{
-    int size;   //size of the queue, from user
-    int front;  //points to the front of the queue
-    int rear;   // points to the rear of the queue
-    struct Node **arr;   // array containing the elements
-};
+// FUNCTIONS
 
-// FUNCTIONS: QUEUE and BST
-
-    // ----------QUEUE---------------------
-
-/*
- * Creates the queue using a dynamically allocated
- * array and initializes the DS parameters.
- * Both front and rear starts at 0.
- */
-void createQ(struct Queue *q){
-    printf("%s\n", "Enter the size of the queue:");
-    scanf("%d", &(q->size));
-
-    q->arr = (int**) malloc (q->size * sizeof(struct Node* ));
-    if (q->arr == NULL) exit (-1);
-
-    q->front = 0;
-    q->rear = 0;
-}
-
-/*
- * Inserts (Enqueues) a given element at the rear of
- * the circular queue.
- */
-void enqueue(struct Queue *q, struct Node *x){
-    if (((q->rear +1) % q->size )== q->front){
-        printf("%s\n", "Queue is Full");
-        return;
-    }
-    else {
-        q->rear = (q->rear + 1) % q->size;
-        q->arr[q->rear] = x;
-    }
-}
-
-/*
- * Deletes (Dequeues) the element at the front of the
- * circular queue.
- */
-struct Node* dequeue(struct Queue *q){
-    struct Node *ret = NULL;
-    if (q->front == q->rear){
-        printf("%s\n", "Queue is Empty");
-    }
-    else {
-        q->front = (q->front + 1) % (q->size);
-        ret = q->arr[q->front];
-    }
-    return ret; // -1 if empty
-}
-
-/*
- * Checks if the queue is empty and returns a boolean.
- * The queue is empty only when front == rear.
- * Returns:
- *      1 = Empty
- *      0 = Not Empty
- */
-int isEmpty(struct Queue q){
-    return (q.front == q.rear);
-}
-
-
-    // ----------BST------------------
 
 /*
  * Initializes a Binary Search Tree
@@ -100,58 +30,7 @@ void Init_bst(struct Node **root){
     *root = NULL;
 }
 
-/*
- * Create a BST asking for each node's value
- * starting from the root node and then following
- * from top to bottom and from left to right.
- * Enter -1 if the node is missing, you will be
- * asked to enter a node number until a full level
- * has been entered.
- *
- * Uses a Circular Queue to keep track of the nodes
- * that have been entered.
- */
-void create_bst(struct Node **root){
-    struct Node *p, *t;
-    int x;
-    struct Queue q;
 
-    createQ(&q);
-
-    printf("%s\n", "Enter value of root:");
-    scanf("%d", &x);
-    *root = (struct Node *) malloc(sizeof(struct Node));
-    if (*root == NULL) exit (-1);
-    (*root)->data = x;
-    (*root)->lchild = (*root)->rchild=NULL;
-    enqueue(&q, *root);
-
-    while (!isEmpty(q)){
-        p = dequeue(&q);
-        printf("Enter left child of %d [-1 if not present]:\n", p->data);
-        scanf("%d", &x);
-        if (x != -1){
-            t = (struct Node*) malloc (sizeof(struct Node));
-            if (t == NULL) exit (-1);
-            t->data = x;
-            t->lchild = t->rchild = NULL;
-            p->lchild = t;
-            enqueue(&q, t);
-        }
-        printf("Enter right child of %d [-1 if not present]:\n", p->data);
-        scanf("%d", &x);
-        if (x != -1){
-            t = (struct Node*) malloc (sizeof(struct Node));
-            if (t == NULL) exit (-1);
-            t->data = x;
-            t->lchild = t->rchild = NULL;
-            p->rchild = t;
-            enqueue(&q, t);
-        }
-
-    }
-}
-f
 /*
  * Prints the values of the nodes in Preorder
 */
@@ -185,25 +64,151 @@ void inorder(struct Node *p){
     }
 }
 
+/*
+ * Searches for a value in the given tree.
+ * If found returns the node's address, otherwise
+ * returns NULL.
+ * RECURSIVE VERSION
+ *
+ */
+struct Node* BST_search(struct Node *q, int key){
+    if (q == NULL){
+        return NULL;
+    }
 
-// MAIN ---------------------------------------
+    if (q->data == key){
+        return q;
+    }
+    else if (key < q->data){
+        return BST_search(q->lchild, key);
+    }
+    else{
+        return BST_search(q->rchild, key);
+    }
+
+
+    return NULL;
+}
 
 /*
- * Everything inside main is used
- * to experiment with the code and for
- * debugging purposes
+ * Searches for a value in the given tree.
+ * If found returns the node's address, otherwise
+ * returns NULL.
+ * ITERATIVE VERSION
+ *
+ */
+struct Node* BST_search_iter(struct Node *q, int key){
+
+    while (q){
+        if (q->data == key){
+        return q;
+        }
+        else if (key < q->data){
+            q = q->rchild;
+        }
+        else if (key > q->data){
+            q = q->rchild;
+        }
+    }
+
+    return NULL;
+}
+
+/*
+ * Auxillary function to BST_insert that creates
+ * a node in case the tree is empty
+ */
+struct Node* create_node(int key){
+
+    struct Node* t = (struct Node*) malloc (sizeof(struct Node));
+    if (!t) {
+        puts("Error, malloc overflow");
+        exit (-1);
+    }
+    t->data = key;
+    t->lchild = t->rchild = NULL;
+    return t;
+
+};
+
+/*
+ * Inserts a node in a BST
+ * Uses the auxillary function create_node
+ * NB Using struct Node *node and not **node as the
+ * ** are required only if I intend to modify the pointer
+ * I am passing to the function!
+ *
+ * Also note the need to "start" the tree when calling the
+ * function the first time with something like:
+ * first = BST_insert(NULL, int);
+ *
+ */
+struct Node* BST_insert(struct Node *node, int key){
+
+    if (node == NULL){
+        return create_node(key);
+//  What follows in the comments has been replaced by
+//  the above line that invokes create_root
+//        struct Node *t;
+//        t = (struct Node*) malloc (sizeof(struct Node));
+//        t->data = key;
+//        t->lchild = t->rchild = NULL;
+//        return t;
+    }
+    if (key < node->data){
+        node->lchild = BST_insert(node->lchild, key);
+    }
+    else if (key > node->data){
+        node->rchild = BST_insert(node->rchild, key);
+    }
+    return node;
+}
+
+ /*
+  * Utility function, used by BST_delete, that
+  * finds the inorder predecessor of the given node
+  * and returns it.
+  */
+struct Node* inorder_predecessor(struct Node *node){
+    if (node == NULL)
+        return node;
+
+    if (node->lchild == NULL && node->rchild == NULL){
+        return node->lchild;
+    }
+    else {
+        struct Node *p = node->lchild;
+        while (p->rchild != NULL){
+            p = p->rchild;
+        }
+        return p;
+    }
+}
+
+// BST_delete to be implemented
+
+
+// MAIN
+
+/*
+ * Everything in main is just for testing
+ * purposes.
  */
 int main (void){
 
-    struct Node *root;
-    Init_bst(&root);
+    struct Node *first;
+    Init_bst(&first);
+    first = BST_insert(first, 15); // same as first = BST_insert(NULL, 15);
+    BST_insert(first, 5);
+    BST_insert(first, 3);
+    BST_insert(first, 12);
+    BST_insert(first, 13);
+    BST_insert(first, 20);
+    BST_insert(first, 35);
+    BST_insert(first, 40);
+    struct Node *x = inorder_predecessor(first);
+    printf("pred -> %d\n", x->data);
 
-    create_bst(&root);
-
-    preorder(root);
-
-    //inorder(root);
-
-    //postorder(root);
+    preorder(first);
     return 0;
 }
